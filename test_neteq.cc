a1ab8603b37c481aa7cd3bac9f11082d6a03f0a1
yswan <wyuansheng@126.com>
2022-10-03 16:26:44 +0800

    insert packet

--- test_neteq.cc
+++ test_neteq.cc
@@ -53,16 +53,57 @@ std::unique_ptr<AudioMultiVector> algorithm_buffer_;
 std::unique_ptr<SyncBuffer> sync_buffer_;
 
 bool first_packet_ = true;
-uint32_t timestamp_;
+uint32_t timestamp_ = 0;
 uint32_t playout_timestamp_;
 
-int fs_hz = 48000;
+int fs_hz_ = 48000;
 size_t channels = 1;
-int fs_mult_ = fs_hz / 8000;
+int fs_mult_ = fs_hz_ / 8000;
 int samples_10ms = static_cast<size_t>(10 * 8 * fs_mult_);
 int input_size_samples_ = static_cast<size_t>(kInputSizeMs * 8 * fs_mult_);
 int output_size_samples_ = static_cast<size_t>(kOutputSizeMs * 8 * fs_mult_);
 int decoder_frame_length_ = 3 * output_size_samples_;
+Modes last_mode_ = kModeNormal;
+
+void init_param(){
+    fs_hz_ = 48000;
+    channels = 1;
+    fs_mult_ = fs_hz_ / 8000;
+    samples_10ms = static_cast<size_t>(10 * 8 * fs_mult_);
+    input_size_samples_ = static_cast<size_t>(kInputSizeMs * 8 * fs_mult_);
+    output_size_samples_ = static_cast<size_t>(kOutputSizeMs * 8 * fs_mult_);
+    decoder_frame_length_ = 3 * output_size_samples_;
+    last_mode_ = kModeNormal;
+}
+
+struct RTPHeader {
+  RTPHeader() = default;
+  RTPHeader(const RTPHeader& other) = default;
+  RTPHeader& operator=(const RTPHeader& other) = default;
+  uint8_t payloadType{0};
+  uint16_t sequenceNumber{0};
+  uint32_t timestamp{0};
+};
+
+  enum ErrorCodes {
+    kNoError = 0,
+    kOtherError,
+    kUnknownRtpPayloadType,
+    kDecoderNotFound,
+    kInvalidPointer,
+    kAccelerateError,
+    kPreemptiveExpandError,
+    kComfortNoiseErrorCode,
+    kDecoderErrorCode,
+    kOtherDecoderError,
+    kInvalidOperation,
+    kDtmfParsingError,
+    kDtmfInsertError,
+    kSampleUnderrun,
+    kDecodedTooMuch,
+    kRedundancySplitError,
+    kPacketBufferCorruption
+  };
 
 void init_eq()
 {
@@ -79,18 +120,18 @@ void init_eq()
     expand_factory_.reset(new ExpandFactory);
     expand_.reset(expand_factory_->Create(background_noise_.get(),
                     sync_buffer_.get(), &random_vector_,
-                    fs_hz, channels));
+                    fs_hz_, channels));
 
-    merge_.reset(new Merge(fs_hz, channels, expand_.get(), sync_buffer_.get()));
+    merge_.reset(new Merge(fs_hz_, channels, expand_.get(), sync_buffer_.get()));
 
-    normal_.reset(new Normal(fs_hz, nullptr, *background_noise_, expand_.get()));
+    normal_.reset(new Normal(fs_hz_, nullptr, *background_noise_, expand_.get()));
 
     accelerate_factory_.reset(new AccelerateFactory);
-    accelerate_.reset(accelerate_factory_->Create(fs_hz, channels, *background_noise_));
+    accelerate_.reset(accelerate_factory_->Create(fs_hz_, channels, *background_noise_));
 
     preemptive_expand_factory_.reset(new PreemptiveExpandFactory);
     preemptive_expand_.reset(preemptive_expand_factory_->Create(
-        fs_hz, channels, *background_noise_, expand_->overlap_length()));
+        fs_hz_, channels, *background_noise_, expand_->overlap_length()));
 
     tick_timer_.reset(new TickTimer);
     delay_peak_detector_.reset(new DelayPeakDetector(tick_timer_.get()));
@@ -98,7 +139,7 @@ void init_eq()
 
     buffer_level_filter_.reset(new BufferLevelFilter);
 
-	packet_buffer_.reset(new PacketBuffer(kMaxPacketsInBuffer));
+    packet_buffer_.reset(new PacketBuffer(kMaxPacketsInBuffer));
     decoded_buffer_.reset(new int16_t[decoded_buffer_length_]);
     // Delete algorithm buffer and create a new one.
     algorithm_buffer_.reset(new AudioMultiVector(channels));
@@ -107,6 +148,69 @@ void init_eq()
     // Move index so that we create a small set of future samples (all 0).
     sync_buffer_->set_next_index(sync_buffer_->next_index() -
                     expand_->overlap_length());
+}
+
+int InsertPacket(const RTPHeader& rtp_header,
+                    rtc::ArrayView<const uint8_t> payload,
+                    uint32_t receive_timestamp) 
+{
+    if (payload.empty()){
+        return kInvalidPointer;
+    }
+
+    Packet packet;
+    packet.payload_type = rtp_header.payloadType;
+    packet.sequence_number = rtp_header.sequenceNumber;
+    packet.timestamp = rtp_header.timestamp;
+
+    // Store these for later use, since the first packet may very well disappear
+    // before we need these values.
+    uint32_t main_timestamp = packet.timestamp;
+    uint8_t main_payload_type = packet.payload_type;
+    uint16_t main_sequence_number = packet.sequence_number;
+
+    bool update_sample_rate_and_channels = first_packet_;
+
+    if (update_sample_rate_and_channels){
+
+        packet_buffer_->Flush();
+
+        sync_buffer_->IncreaseEndTimestamp(main_timestamp - timestamp_);
+
+        timestamp_ = main_timestamp;
+    }
+
+    const int ret = packet_buffer_->InsertPacket(std::move(packet));
+    if (ret == PacketBuffer::kFlushed) {
+        // Reset DSP timestamp etc. if packet buffer flushed.
+        update_sample_rate_and_channels = true;
+    } else if (ret != PacketBuffer::kOK) {
+        return kOtherError;
+    }
+
+    if (first_packet_) {
+        first_packet_ = false;
+    }
+
+    delay_manager_->LastDecodedWasCngOrDtmf(0/*dec_info->IsComfortNoise() ||
+                                            dec_info->IsDtmf()*/);
+    if (delay_manager_->last_pack_cng_or_dtmf() == 0) {
+
+        // Update statistics.
+        if ((int32_t)(main_timestamp - timestamp_) >= 0/* && !new_codec_*/) {
+            // Only update statistics if incoming packet is not older than last played
+            // out packet, and if new codec flag is not set.
+            delay_manager_->Update(main_sequence_number, main_timestamp, fs_hz_);
+        }
+    } else if (delay_manager_->last_pack_cng_or_dtmf() == -1) {
+        // This is first "normal" packet after CNG or DTMF.
+        // Reset packet time counter and measure time until next packet,
+        // but don't update statistics.
+        delay_manager_->set_last_pack_cng_or_dtmf(0);
+        delay_manager_->ResetPacketIatCount();
+    }
+
+    return 0;
 }
 
 int main(){
